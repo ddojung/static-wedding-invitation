92ad97402cc46cf8b1e7f9caa75c0e0e1d74bd1e
ddojung <0901sj@gmail.com>
2020-04-09 15:03:07 +0900

    Move marker center & Remove docs directory

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ const map = new naver.maps.Map('map', mapOptions);
 
 const markerOptions: naver.maps.MarkerOptions = {
   map,
-  position: { lat: 37.6675781, lng: 126.9529092 },
+  position: { lat: 37.6682521, lng: 126.9495151 },
   icon: {
     url: 'https://firebasestorage.googleapis.com/v0/b/wedding-invi.appspot.com/o/map-marker.svg?alt=media&token=dcd71850-1cd8-431e-9322-d9ba87ac948e',
     size: new naver.maps.Size(35, 35),
